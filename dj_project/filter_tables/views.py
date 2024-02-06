07efecb43ffe96d202537bc07131166c2e8852e4
AndreKuratomi <andrekuratomi@gmail.com>
2024-02-06 17:22:39 -0300

    feat: templates separated and views updated for it.

--- dj_project/filter_tables/views.py
+++ dj_project/filter_tables/views.py
@@ -85,75 +85,84 @@ class EmailAttachByTable(APIView):
                     valor_liquido = row.valor_liquido
                     vencimento = row.dt_vencto
 
-                    row_data = {"cnpj": cnpj, "nfe": nfe, "razao_social": razao_social, "valor_liquido": valor_liquido, "vencimento": vencimento}
+                    row_data = {
+                        "cnpj": cnpj, 
+                        "nfe": nfe, 
+                        "razao_social": razao_social, 
+                        "valor_liquido": valor_liquido, 
+                        "vencimento": vencimento, 
+                        "receiver_email": "andrekuratomi@gmail.com"
+                    }
 
-                    # TAKING INPUT IDS WITH SELENIUM ROBOT:
-                    input_ids = recursive_robot(username, sharepoint_url)
-                    print(input_ids)
+                    # USERNAME AND EMAIL TO WORK WITH:
+                    # data={'competencia_por_ano': "", 'nfe': "", 'nome_do_cliente': "", , 'tipo_de_servico': ""}
                     
-                    # PLACING TABLE TO WORK WITH WITH SELENIUM ROBOT:
-                    robot_for_sharepoint(username, password, input_ids["user_input_id"], input_ids["password_input_id"], sharepoint_url, download_directory, "02390435000115", "17779")
-                    # robot_for_sharepoint(username, password, input_ids["user_input_id"], input_ids["password_input_id"], sharepoint_url, download_directory, row_data["cnpj"], row_data["nfe"])
+                    # serializer = EmailSerializer(data)
+                    # # print(serializer)
+                    # if not serializer.is_valid():
+                    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                    
+                    # Attach files to email:
+                    path = Path("./robot_sharepoint/attachments/")
+                    print(path)
+                    tables_path_content = list(path.iterdir())
 
-                    print("here i am!")
-                    SendEmailView(APIView)
-                    # response = requests.post("<my_powerautomate_http_endpoint>", json=row_data)
+                    table_template = ""
+                    if tables_path_content.count(3):
+                        table_template = "table_template_boleto.html"
+                    else:
+                        table_template = "table_template_deposito.html"
 
-                    # if response.status_code == 200:
-                    #     print("Flow working!")
-                    # else:
-                    #     print(f"Error! Status code {response.status_code}")
-                    # counter += 1
+                    print(table_template)
 
-                    # print("Email successfully sent! Check inbox.")
+                    # Insert table to mail body:
+                    mail_content = render_to_string(
+                        table_template, {
+                            # 'competencia_por_ano': row_data['competencia_por_ano'], 
+                            'nfe': row_data['nfe'], 
+                            # 'nome_do_cliente': row_data['nome_do_cliente'], 
+                            'receiver_email': row_data['receiver_email'], 
+                            # 'tipo_de_servico': row_data['tipo_de_servico']
+                        }
+                        #  , using='ISO-8859-1'
+                    )
+                    # print(mail_content)
+                    time.sleep(2)  # wait for file to be created
+                    ipdb.set_trace()
+                    email = EmailMessage(
+                        "Nota Fiscal Eletrônica - J&C Faturamento - {a1}  {a2}  ( {a3} )  NF -  -  - {a4}"
+                        .format(
+                            # a1=row_data['tipo_de_servico'], 
+                            # a2=row_data['competencia_por_ano'], 
+                            # a3=row_data['nome_do_cliente'], 
+                            a4=row_data['nfe']
+                        ),
+                        # "Envio tabela  {a1} - Novelis".format(a1=row_data['receiver_name']),
+                        "",
+                        "{}".format(host_email), 
+                        [row_data['receiver_email']], 
+                        fail_silently=False,
+                        html_message=mail_content
+                    )
 
-            return Response({"message": "Email successfully sent"}, status=status.HTTP_200_OK)
-  
-        except:
-            return Exception({"error": "Something went wrong! Contact the dev!"})
+                    for file in tables_path_content:
+                        print(file)
+                        str(file)
+                        email.attach_file(file)
 
-class SendEmailView(APIView):
-    def post(self):
-        try: 
-            # USERNAME AND EMAIL TO WORK WITH:
-            data={'competencia/ano': "", 'nfe': "", 'nome_do_cliente': "", 'receiver_email': "andrekuratomi@gmail.com", 'tipo_de_servico': ""}
-            
-            # serializer = EmailSerializer(data)
-            # # print(serializer)
-            # if not serializer.is_valid():
-            #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                    email.send()
+                    print("Email successfully sent! Check inbox.")
 
-            # Insert table to mail body:
-            mail_content = render_to_string('table_template.html', {'competencia/ano': data['competencia/ano'], 'nfe': data['nfe'], 'nome_do_cliente': data['nome_do_cliente'], 'receiver_email': data['receiver_email'], 'tipo_de_servico': data['tipo_de_servico']}
-                                            #  , using='ISO-8859-1'
-                                            )
-            # print(mail_content)
-            time.sleep(2)  # wait for file to be created
+                    # return Response({"message": "Email successfully sent"}, status=status.HTTP_200_OK)
 
-            email = EmailMessage(
-                "Nota Fiscal Eletrônica - J&C Faturamento - {a1}  {a2}  ( {a3} )  NF -  -  - {a4}".format(a1=data['tipo_de_servico'], a2=data['competencia/ano'], a3=data['nome_do_cliente'], a4=data['nfe']),
-                # "Envio tabela  {a1} - Novelis".format(a1=data['receiver_name']),
-                "",
-                "{}".format(host_email), 
-                [data['receiver_email']], 
-                fail_silently=False,
-                html_message=mail_content
-            )
-            
-            # Attach files to email:
-            path = Path("../../robot_sharepoint/attachments/")
-            tables_path_content = list(path.iterdir())
+        except Exception as e:
+            print(f"error:Something went wrong! {e} Contact the dev!")
+            # return Exception({"error": "Something went wrong! Contact the dev!"})
 
-            for file in tables_path_content:
-                print(file)
-                str(file)
-                email.attach_file(file)
 
-            email.send()
-
-            print("Email successfully sent! Check inbox.")
-
-            return Response({"message": "Email successfully sent"}, status=status.HTTP_200_OK)
+# class SendEmailView(APIView):
+#     def post(self, row_data):
+#         print("I")
+#         ipdb.set_trace()
+#         print("AM")
         
-        except:
-            return Exception({"error": "Something went wrong! Contact the dev!"})
