907f00b0e8c4ab392d339491d9fc5db0750d115e
andrekura <andre.kuratomi@jcgestaoderiscos.com.br>
2024-02-23 12:57:10 -0300

    refactor: more envs moved to file and cleancode

--- run_everything_here.py
+++ run_everything_here.py
@@ -4,16 +4,17 @@ import sys
 import django
 import ipdb
 
-from utils.envs import username, password, sharepoint_url
 
 # Preparing django to run outside its dir:
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'novelis_table_filter_mail_project.settings')
 sys.path.append("./dj_project")
 django.setup()
 
-from management_before_django.table_managements.scripts import tables_to_db
 from dj_project.filter_tables.views import EmailAttachByTable
+from management_before_django.table_managements.scripts import tables_to_db
 from robot_sharepoint.modules.robot_to_upload_files import upload_files_to_sharepoint
+from utils.envs import username_test, password_test, sharepoint_for_upload_url
+from utils.paths import reports_path
 
 root_directory = os.path.dirname(os.path.abspath(__file__))
 root_directory = str(root_directory)
@@ -27,5 +28,5 @@ try:
 except Exception as e: 
     print(f"PROCESSO INTERROMPIDO! Error: {e} CONTATAR DEV RESPONSÁVEL \n Mas pode continuar.")
 finally: 
-    upload_files_to_sharepoint(username, password, sharepoint_url)
+    upload_files_to_sharepoint(username_test, password_test, reports_path, sharepoint_for_upload_url)
   
--- utils/envs.py
+++ utils/envs.py
@@ -7,6 +7,8 @@ load_dotenv()
 # Keys for login:
 username = os.getenv("USERN")
 password = os.getenv("PASSWORD")
+username_test = os.getenv("EMAIL_HOST_USER_TEST")
+password_test = os.getenv("EMAIL_HOST_PASSWORD_TEST")
 
 # Input ids:
 hover_selector = os.getenv("HOVER_SELECTOR")
@@ -15,3 +17,4 @@ download_selector = os.getenv("DOWNLOAD_SELECTOR")
 host_email = os.getenv("EMAIL_HOST_USER")
 download_directory = os.getenv("DOWNLOAD_DIRECTORY")
 sharepoint_url = os.getenv("SHAREPOINT_URL")
+sharepoint_for_upload_url = os.getenv("SHAREPOINT_FOR_UPLOAD_URL")
