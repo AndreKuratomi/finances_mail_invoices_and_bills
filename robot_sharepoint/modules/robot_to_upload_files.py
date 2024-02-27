55026d0f094d7a3ebb327ecdcc9d005783132f88
andrekura <andre.kuratomi@jcgestaoderiscos.com.br>
2024-02-27 18:45:21 -0300

    feat: single report with both sent and not sent elements upload drafted.

--- robot_sharepoint/modules/robot_to_upload_files.py
+++ robot_sharepoint/modules/robot_to_upload_files.py
@@ -55,46 +55,21 @@ def upload_files_to_sharepoint(username: str, password: str, reports_path: Path,
 
     # Dialog page!:
     file_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[type='file']")))
-    # driver.find_element(By.CSS_SELECTOR, "input[type='file']")
     
     # Extract info from attachments:
     tables_path_content = list(Path(reports_path).iterdir())    
     # Attach files to email:
     for file in tables_path_content:
-        print(file)
         stringfy = str(file.resolve())
-        print(stringfy)
-        if stringfy.endswith(".txt"):
+        print('file:', file)
+        print('stringfy:', stringfy)
+        if stringfy.endswith("reports_list.txt"):
             try:
                 time.sleep(1)
                 file_input.send_keys(stringfy)
-                print("OK!")
-                ipdb.set_trace()
                 time.sleep(3)
                 
-            except StaleElementReferenceException:
-                ipdb.set_trace()
-                file_input.send_keys(Keys.ESCAPE)
-                print("IRK!")
-                item = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "i[data-icon-name='More']")))
-                time.sleep(1)
-                item.click()
-
-                upload = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "li[title='Carregar arquivos do seu computador para este local']")))
-
-                # Create an instance of ActionChains and perform the hover action
-                actions = ActionChains(driver)
-                actions.move_to_element(upload).perform()
-                item2 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button[name='Arquivos']")))
-                item2.click()
-                time.sleep(2)
-
-                # Dialog page!:
-                file_input = driver.find_element(By.CSS_SELECTOR, "input[type='file']")
-                # file_input = driver.find_element(By.CSS_SELECTOR, "input[type='file']")
-                file_input.send_keys(stringfy)
-                print("OK!2")
-                file_input.send_keys(Keys.ESCAPE)
-    # ipdb.set_trace()
+            except Exception as e:
+                print(e) 
 
     driver.quit()
--- run_everything_here.py
+++ run_everything_here.py
@@ -17,15 +17,14 @@ from management_before_django.table_managements.scripts import tables_to_db
 from robot_sharepoint.modules.robot_to_upload_files import upload_files_to_sharepoint
 from robot_sharepoint.modules.robot_for_login_and_download_raw_table import robot_for_raw_table
 from robot_sharepoint.modules.download_directories_management import check_if_dir_is_empty_or_not
+from robot_sharepoint.modules.join_reports import join_reports
 
 from utils.envs import download_directory, username_test, password_test, raw_table_directory, sharepoint_for_upload_url
 from utils.paths import reports_path
+from utils.report_files import not_found_list, sent_list, elements_reports_list, sent_title
 
 root_directory = os.path.dirname(os.path.abspath(__file__))
 root_directory = str(root_directory)
-
-
-tables_to_db.tables_to_db()
 
 do_we_have_table_to_work_with = check_if_dir_is_empty_or_not(raw_table_directory)
 
@@ -33,18 +32,17 @@ if not do_we_have_table_to_work_with:
     print("Coming soon...")
     # robot_for_raw_table(download_directory, username_test, password_test, sharepoint_for_upload_url)
     
-    # # Raw reports creation:
-    # with reports_path.joinpath(final_not_found_list).open("w") as file:
-    # file.write(not_found_list)
-    
-    # # ipdb.set_trace()
-    # with reports_path.joinpath(final_sent_list).open("w") as file:
-    #     file.write(sent_list)
+    # Raw reports creation:
+    with reports_path.joinpath(sent_list).open("w") as file:
+        file.write(sent_title)
+
 else:
     try:
+        tables_to_db.tables_to_db()
         EmailAttachByTable().post(root_directory)
     except Exception as e: 
         print(f"PROCESSO INTERROMPIDO! Error: {e} CONTATAR DEV RESPONSÁVEL \n Mas pode continuar.")
     finally: 
+        join_reports(not_found_list, sent_list, elements_reports_list, reports_path)
         upload_files_to_sharepoint(username_test, password_test, reports_path, sharepoint_for_upload_url)
     
