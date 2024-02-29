8a6de326f55763fc4178d622d73e3b35799e33fd
andrekura <andre.kuratomi@jcgestaoderiscos.com.br>
2024-02-29 15:21:48 -0300

    refactor: envs and paths renamed; 'guambiarra' made for long envs.

--- utils/report_files.py
+++ utils/report_files.py
@@ -1,8 +1,12 @@
+import datetime
+
+current = datetime.datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
+
 # Raw text files:
-elements_reports_list = "elements_reports_list.txt"
-not_found_list = "not_found_list.txt"
-sent_list = "sent_list.txt"
+elements_reports_list = f"{current} - relatorio diário.txt"
+not_found_list = "faturamentos_nao_encontrados.txt"
+sent_list = "faturamentos_enviados.txt"
 
 # Report titles:
-not_found_title = "Not found elements: \n"
-sent_title = "Sent elements: \n"
+not_found_title = "Faturamentos não encontrados: \n"
+sent_title = "Faturamentos enviados: \n"
