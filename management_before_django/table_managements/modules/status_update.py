665a83686011910c4335ea122a759a6a33469aa3
andrekura <andre.kuratomi@jcgestaoderiscos.com.br>
2024-03-05 12:39:12 -0300

    refactor: paths_with_file_name() created to improve legibility.

--- management_before_django/table_managements/modules/status_update.py
+++ management_before_django/table_managements/modules/status_update.py
@@ -1,77 +1,51 @@
 from openpyxl import load_workbook
 
-import pandas as pd
 from pathlib import Path
-from tqdm import tqdm
 
-from errors.custom_exceptions import TooManyFilesError
-from utils.variables.paths import edited_tables_path
+from management_before_django.table_managements.modules.take_path_from_directory import paths_with_file_name
 
 import ipdb
 
 
-def status_update(path: Path, row_data: dict) -> None:
+def status_update(edited_path: Path, row_data: dict) -> None:
     """Receives the tables' path and row_data as dict, searches for the original row by NFE value and if found updates status to 'Enviado'."""
 
-    # Checking path content:
-    tables_path_content = list(path.iterdir())  
-    tables = list()
+    (complete_file_path_to_edited, file_path_to_edited) = paths_with_file_name(edited_path)
 
-    for elem in tables_path_content:
-        stringfied_elem = str(elem)
-        if stringfied_elem.endswith('.xls') or stringfied_elem.endswith('.xlsx') or stringfied_elem.endswith('.xlsm'):
-            tables.append(stringfied_elem)
-        
-    if len(tables) == 0:
-        raise FileNotFoundError("NO TABLE TO WORK WITH!")
-    elif len(tables) > 1:
-        raise TooManyFilesError
-    
-    # Working with it:
-    for file in tables_path_content:
-        if file.is_file():
-            path_to_table = str(file)
-            if path_to_table.endswith('.xls') or path_to_table.endswith('.xlsx') or path_to_table.endswith('.xlsm'):
+    # OPENPYXL TO ADD STATUS COLUMN:
+    workbook = load_workbook(data_only=True, filename=file_path_to_edited)
+    worksheet = workbook.active
 
-                # OPENPYXL TO ADD STATUS COLUMN:
-                workbook = load_workbook(data_only=True, filename=path_to_table)
-                worksheet = workbook.active
+    # Find column index for NFE ('Numero'):
+    column_index_for_nfe = None
+    column_index_for_status = None
 
-                # Find column index for NFE ('Numero'):
-                column_index_for_nfe = None
-                column_index_for_status = None
+    for cell in worksheet[1]:
+        # print("cell.value:", cell.value)
+        if cell.value == 'Numero':
+            # ipdb.set_trace()
+            column_index_for_nfe = cell.column
+            # print("cell.column:", cell.column)
+            # print(column_index)
+        elif cell.value == "STATUS":
+            column_index_for_status = cell.column
 
-                for cell in worksheet[1]:
-                    # print("cell.value:", cell.value)
-                    if cell.value == 'Numero':
-                        # ipdb.set_trace()
-                        column_index_for_nfe = cell.column
-                        # print("cell.column:", cell.column)
-                        # print(column_index)
-                    elif cell.value == "STATUS":
-                        column_index_for_status = cell.column
+    if column_index_for_nfe is None or column_index_for_status is None:
+        raise ValueError("Column 'Numero' not found in the worksheet")
+    else:
+        try:
+            for row_to_update in range(2, worksheet.max_row + 1): 
+                like_ancient_nfe = '0' + row_data['nfe']
+                if worksheet.cell(row=row_to_update, column=column_index_for_nfe).value == str(like_ancient_nfe):
+                    print("row_data['nfe']:", row_data['nfe'])
+                    worksheet.cell(row=row_to_update, column=column_index_for_status).value = "Enviado"
+                    # ipdb.set_trace()
+                    print("worksheet.cell(row=row_to_update, column=column_index_for_status).value:", worksheet.cell(row=row_to_update, column=column_index_for_status).value)
+                    break
 
-                if column_index_for_nfe is None or column_index_for_status is None:
-                    raise ValueError("Column 'Numero' not found in the worksheet")
+            workbook.save(complete_file_path_to_edited)
+            workbook.close()
 
-                try:
-                    for row_to_update in range(2, worksheet.max_row + 1): 
-                        like_ancient_nfe = '0' + row_data['nfe']
-                        if worksheet.cell(row=row_to_update, column=column_index_for_nfe).value == str(like_ancient_nfe):
-                            print("row_data['nfe']:", row_data['nfe'])
-                            worksheet.cell(row=row_to_update, column=column_index_for_status).value = "Enviado"
-                            # ipdb.set_trace()
-                            print("worksheet.cell(row=row_to_update, column=column_index_for_status).value:", worksheet.cell(row=row_to_update, column=column_index_for_status).value)
-                            break
-                        path_back = str(edited_tables_path.resolve() / file.name)
-                        print("path_back:", path_back)
-
-                    workbook.save(path_back)
-                    workbook.close()
-                    
-                except Exception as e:
-                    raise FileNotFoundError("No row with this NFE found!")
-                    print(e)
-
-        else:
-            raise Exception("Something went wrong with this file... ")
+        except Exception as e:
+            print(e)
+            raise FileNotFoundError("No row with this NFE found!")
--- management_before_django/table_managements/modules/table_columns_edition.py
+++ management_before_django/table_managements/modules/table_columns_edition.py
@@ -1,125 +1,102 @@
-from openpyxl import load_workbook
-
 import pandas as pd
+import time
+
+from openpyxl import load_workbook
 from pathlib import Path
 from tqdm import tqdm
 
-from errors.custom_exceptions import TooManyFilesError
-
 from management_before_django.table_managements.modules.compare_spreadsheets import compare_spreadsheets
+from management_before_django.table_managements.modules.take_path_from_directory import paths_with_file_name
 
 from utils.functions.delete_elements import do_we_have_things_to_delete
 from utils.functions.path_length import do_we_have_spreadsheets
-from utils.variables.paths import edited_tables_path, raw_tables_path
-from utils.variables.envs import sheet
 
 import ipdb
 
 
-def filter_table_column(path: Path, sheet: str) -> pd.DataFrame:
+def filter_table_column(raw_path: Path, edited_path: Path, sheet: str) -> pd.DataFrame:
     """Receives the tables' path, filters it as necessary and inserts it to Pandas dataframe"""
 
-    # Checking path content:
-    tables_path_content = list(path.iterdir())  
-    tables = list()
+    (complete_file_path_to_raw, file_path_to_raw) = paths_with_file_name(raw_path)
 
-    for elem in tables_path_content:
-        stringfied_elem = str(elem)
-        if stringfied_elem.endswith('.xls') or stringfied_elem.endswith('.xlsx') or stringfied_elem.endswith('.xlsm'):
-            tables.append(stringfied_elem)
+    # OPENPYXL TO ADD STATUS COLUMN:
 
-    if len(tables) == 0:
-        raise FileNotFoundError("NO TABLE TO WORK WITH!")
-    elif len(tables) > 1:
-        raise TooManyFilesError
-    
-    # Working with it:
-    for file in tables_path_content:
-        if file.is_file():
+    workbook = load_workbook(data_only=True, filename=file_path_to_raw)
+    attempt = workbook.active
 
-            path_to_raw = str(raw_tables_path.resolve() / file.name)
-            path_to_edited = str(edited_tables_path.resolve() / file.name)
+    col_names = [col.value for col in attempt[1]]
+    # do_we_have_status = [elem.value for elem in col_names if elem.value = "STATUS"]
 
-            path_to_table = str(file)
-            if path_to_table.endswith('.xls') or path_to_table.endswith('.xlsx') or path_to_table.endswith('.xlsm'):
+    if "STATUS" not in col_names:
+        new_column = attempt.max_column + 1
 
-                # OPENPYXL TO ADD STATUS COLUMN:
-                workbook = load_workbook(data_only=True, filename=path_to_table)
-                attempt = workbook.active
+        attempt.insert_cols(new_column)
+        attempt.cell(row=1, column=new_column).value = "STATUS"
 
-                col_names = [col.value for col in attempt[1]]
-                # ipdb.set_trace()
-                # do_we_have_status = [elem.value for elem in col_names if elem.value = "STATUS"]
+        for cell in range(2, attempt.max_row + 1):
+            # print(cell)
+            attempt.cell(row=cell, column=new_column).value = "Não enviado"
 
-                if "STATUS" not in col_names:
-                    new_column = attempt.max_column + 1
+        table_in_edited_table_path = do_we_have_spreadsheets(edited_path)
 
-                    attempt.insert_cols(new_column)
-                    attempt.cell(row=1, column=new_column).value = "STATUS"
+        print("file_path_to_raw:", file_path_to_raw)
 
-                    for cell in range(2, attempt.max_row + 1):
-                        # print(cell)
-                        attempt.cell(row=cell, column=new_column).value = "Não enviado"
-                    
-                    table_in_edited_table_path = do_we_have_spreadsheets(edited_tables_path)
-                    
-                    # print("path_to_edited:", path_to_edited)
-                    # print("path_to_raw:", path_to_raw)
-                    # COMPARE HERE:
-                    if table_in_edited_table_path:
-                        workbook.save(path_to_raw)
-                        workbook.close()
-                        # ipdb.set_trace()
+        # COMPARE HERE:
+        if table_in_edited_table_path:
+            (complete_file_path_to_edited, file_path_to_edited) = paths_with_file_name(edited_path)
+            print("file_path_to_edited:", file_path_to_edited)
+            # Update table just downloaded with column status:
+            workbook.save(file_path_to_raw)
+            workbook.close()
 
-                        compare_spreadsheets(path_to_raw, path_to_raw, sheet)
-                    
-                    else:
-                        workbook.save(path_to_edited)
-                        workbook.close()
+            compare_spreadsheets(file_path_to_raw, file_path_to_edited, complete_file_path_to_edited, sheet)
 
-                # Delete no more necessary raw table 
-                do_we_have_things_to_delete(raw_tables_path, '.xlsx')
-
-                # OPENPYXL TO ADD STATUS COLUMN:
-                # ipdb.set_trace()
-                workbook = load_workbook(data_only=True, filename=path_to_edited)
-                table_sheet = workbook[sheet]
-                # ipdb.set_trace()
-
-                rows = list()
-
-                # First row for titles:
-                headers = [cell.value for cell in table_sheet[1]]
-                headers = [headers[3], headers[4], headers[6],  headers[10], headers[18], headers[23]]
-
-                # Other rows for content:
-                for row in tqdm(table_sheet.iter_rows(min_row=2), "Filtering rows by columns D, E, K, S and X..."):
-                    rows.append([cell.value for i, cell in enumerate(row) if i in [3, 4, 6, 10, 18, 23]]) #IMPROVE
-
-                # PANDAS!
-                df = pd.DataFrame(rows, columns=headers)
-
-                # Drop first row (only for this case!):
-                df.drop(0, inplace=True)
-
-                # Adding column ID to make django work:
-                if 'ID' not in df.columns:
-                    ID = range(1, df.shape[0]+1)
-                    df.insert(0, "id", ID)
-                    df.set_index('id')
-
-                # Editing NFE column to hide first character:
-                df['Numero'] = df['Numero'].apply(lambda x : x[1:])
-
-                # Editing date column to show only date in brazilian format:
-                df['Dt Vencto'] = pd.to_datetime(df['Dt Vencto'], errors='coerce')
-                df['Dt Vencto'] = df['Dt Vencto'].dt.tz_localize('UTC').dt.tz_convert('America/Sao_Paulo')
-                df['Dt Vencto'] = df['Dt Vencto'].dt.strftime('%d/%m/%Y')
-                print(df)
-
-                return df
-
-            else:
-                raise Exception('Only .xls, .xlsx, or .xlsm files are supported.')
         else:
-            raise Exception("Something went wrong with this file... ")
+            edited_file_path = str(edited_path.resolve() / 'edited_table.xlsx')
+            workbook.save(edited_file_path)
+            workbook.close()
+            
+    # Delete no more necessary raw table 
+    # ipdb.set_trace()
+    do_we_have_things_to_delete(raw_path, '.xlsx')
+
+    # OPENPYXL TO ADD STATUS COLUMN:
+    (complete_file_path_to_edited, file_path_to_edited) = paths_with_file_name(edited_path)
+
+    workbook = load_workbook(data_only=True, filename=file_path_to_edited)
+    table_sheet = workbook[sheet]
+    # ipdb.set_trace()
+
+    rows = list()
+
+    # First row for titles:
+    headers = [cell.value for cell in table_sheet[1]]
+    headers = [headers[3], headers[4], headers[6],  headers[10], headers[18], headers[23]]
+
+    # Other rows for content:
+    for row in tqdm(table_sheet.iter_rows(min_row=2), "Filtering rows by columns D, E, K, S and X..."):
+        rows.append([cell.value for i, cell in enumerate(row) if i in [3, 4, 6, 10, 18, 23]]) #IMPROVE
+
+    # PANDAS!
+    df = pd.DataFrame(rows, columns=headers)
+
+    # # Drop first row (only for this case!):
+    # df.drop(0, inplace=True)
+
+    # Adding column ID to make django work:
+    if 'ID' not in df.columns:
+        ID = range(1, df.shape[0]+1)
+        df.insert(0, "id", ID)
+        df.set_index('id')
+
+    # Editing NFE column to hide first character:
+    df['Numero'] = df['Numero'].apply(lambda x : x[1:])
+
+    # Editing date column to show only date in brazilian format:
+    df['Dt Vencto'] = pd.to_datetime(df['Dt Vencto'], errors='coerce')
+    df['Dt Vencto'] = df['Dt Vencto'].dt.tz_localize('UTC').dt.tz_convert('America/Sao_Paulo')
+    df['Dt Vencto'] = df['Dt Vencto'].dt.strftime('%d/%m/%Y')
+    print(df)
+
+    return df
+
