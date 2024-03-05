aef0e6e06a3b71638cd45f44d24bcf8175b467bb
andrekura <andre.kuratomi@jcgestaoderiscos.com.br>
2024-03-05 18:01:22 -0300

    feat: compare_spreadsheets updated and working

--- management_before_django/table_managements/modules/compare_spreadsheets.py
+++ management_before_django/table_managements/modules/compare_spreadsheets.py
@@ -1,19 +1,15 @@
 import pandas as pd
 
 from openpyxl import load_workbook
-from pathlib import Path
-from pandas import DataFrame
-from tqdm import tqdm
-
-from utils.variables.paths import edited_tables_path, raw_tables_path
 
 import ipdb
 
 
-def compare_spreadsheets(path_to_raw: str, path_to_edited: str, sheet: str) -> None:
-    # ipdb.set_trace()
+def compare_spreadsheets(path_to_raw: str, path_to_edited: str, full_path_to_edited: str, sheet: str) -> None:
     updated_workbook = load_workbook(data_only=True, filename=path_to_raw)
     old_workbook = load_workbook(data_only=True, filename=path_to_edited)
+
+    worksheet = old_workbook.active
 
     updated_sheet = updated_workbook[sheet]
     old_sheet = old_workbook[sheet]
@@ -28,13 +24,19 @@ def compare_spreadsheets(path_to_raw: str, path_to_edited: str, sheet: str) -> N
     # Extract data from old table
     for row in old_sheet.iter_rows(min_row=2, values_only=True):
         old_data.append(row)
-
+    print("previous_old_data1:", len(old_data))
+    print("updated_data:", len(updated_data))
     new_lines = [row for row in updated_data if row[6] not in [old_row[6] for old_row in old_data]]
 
     if new_lines:
-        last_row_index = len(old_data) + 2
-        for new_row in new_lines:
-            old_sheet.append(new_row)
-
-    old_workbook.save(path_to_edited)
+        print("LETS WORK!")
+        old_data.extend(new_lines)
+        print("new_old_data2:", len(old_data))
+        
+        for lines in new_lines:
+            old_sheet.append(lines)
+        
+        old_workbook.save(full_path_to_edited)
+        old_workbook.close()
+    # ipdb.set_trace()
     print("DONE!")
